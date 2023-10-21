7288e4d55c6b91c4b7207024bc95d686d0e4cccf
Younes Jarhbou <younes.dev97@gmail.com>
2023-10-21 03:38:56 +0100

    splite task into completed and current

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -11,6 +11,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+
   final TextEditingController _textEditingController = TextEditingController();
 
   void _showAddDialog(BuildContext context) {
@@ -57,37 +59,71 @@ class _HomeState extends State<Home> {
     );
   }
 
+  List<Map<String, dynamic>> getCompletedTasks() {
+    return widget.itemList.where((item) => item['completed']).toList();
+  }
+
+  List<Map<String, dynamic>> getCurrentTasks() {
+    return widget.itemList.where((item) => !item['completed']).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Todo List'),
       ),
-      body: ListView.builder(
-        itemCount: widget.itemList.length,
-        itemBuilder: (context, index) {
-          final item = widget.itemList[index];
-          return ListTile(
-            title: Text(item['text']),
-            subtitle: Text(item['created_at']),
-            trailing: Checkbox(
-              value: item['completed'],
-              onChanged: (value) {
-                // Handle checkbox onChanged event if needed
-                setState(() {
-                  item['completed'] = value;
-                  functionLibrary.sendPutRequest(item['_id']);
-                });
+      body: _currentIndex == 0
+          ? ListView.builder(
+              itemCount: getCurrentTasks().length,
+              itemBuilder: (context, index) {
+                final item = getCurrentTasks()[index];
+                return ListTile(
+                  title: Text(item['text']),
+                  subtitle: Text(item['created_at']),
+                  trailing: Checkbox(
+                    value: item['completed'],
+                    onChanged: (value) {
+                      setState(() {
+                        functionLibrary.sendPutRequest(item['_id']);
+                        item['completed'] = value;
+                        
+                      });
+                    },
+                  ),
+                );
+              },
+            )
+          : ListView.builder(
+              itemCount: getCompletedTasks().length,
+              itemBuilder: (context, index) {
+                final item = getCompletedTasks()[index];
+                return ListTile(
+                  title: Text(item['text']),
+                  subtitle: Text(item['created_at']),
+                  trailing: Icon(Icons.done),
+                );
               },
             ),
-          );
-        },
-      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           _showAddDialog(context);
         },
         child: Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.check_box_outline_blank),
+            label: 'Current Tasks',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.done_all),
+            label: 'Completed Tasks',
+          ),
+        ],
       ),
     );
   }
